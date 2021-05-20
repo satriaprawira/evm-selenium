74e8ea3a0f2494f58d509a203cbc6abea344cb4d
Satria Prawira <satriaprawira19@outlook.com>
2021-05-20 22:37:59 +0700

    Change Chrome WebDriver Version & Renew Elements

--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using NPOI.SS.UserModel;
+using NPOI.SS.Util;
+using NPOI.XSSF.UserModel;
 
 namespace EvermosSelenium
 {
@@ -16,23 +19,16 @@ namespace EvermosSelenium
             Console.Write("Automated Test is started");
 
             //Browser Reference
-            IWebDriver driver = new ChromeDriver();
+
+            IWebDriver driver = new ChromeDriver("D:\\Work\\Automated Test\\Chrome Driver");
+
+            #region Create New Staging Account
 
             //Go to the url
-            driver.Navigate().GoToUrl("https://evermos.com/");
+            driver.Navigate().GoToUrl("https://evermos.com/registration?type=register");
 
-            #region Create New Account
-            IWebElement element = driver.FindElement(By.ClassName("content-daftar"));
-            element.Click();
-
-            element = driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/form/div/div/div[2]/input"));
-            Thread.Sleep(2000);
-            element.SendKeys("08988893419");
-            Thread.Sleep(2000);
-
-            element = driver.FindElement(By.ClassName("pageVerify__submitBtn"));
-            element.Click();
-            Thread.Sleep(2000);
+            IWebElement element = driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div[2]/div/form/div[1]/div[2]/div/div/div[2]/input"));
+            element.SendKeys("89888111112");
 
             driver.Close();
 
