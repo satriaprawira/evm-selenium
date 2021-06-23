539cf7e3d3230b53e538efdd6b32970c1318aa64
Satria Prawira <satriaprawira19@outlook.com>
2021-06-23 20:48:59 +0700

    Add NUnit Test Attributes

--- Program.cs
+++ Program.cs
@@ -9,32 +9,71 @@ using System.Threading.Tasks;
 using NPOI.SS.UserModel;
 using NPOI.SS.Util;
 using NPOI.XSSF.UserModel;
+using NPOI.HSSF.UserModel;
+using System.IO;
+using NUnit.Framework;
 
 namespace EvermosSelenium
 {
     class Program
     {
+        IWebDriver driver = new ChromeDriver("D:\\Work\\Automated Test\\Chrome Driver");
+
         static void Main(string[] args)
         {
-            Console.Write("Automated Test is started");
-
-            //Browser Reference
-
-            IWebDriver driver = new ChromeDriver("D:\\Work\\Automated Test\\Chrome Driver");
-
-            #region Create New Staging Account
-
-            //Go to the url
-            driver.Navigate().GoToUrl("https://evermos.com/registration?type=register");
-
-            IWebElement element = driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div[2]/div/form/div[1]/div[2]/div/div/div[2]/input"));
-            element.SendKeys("89888111112");
-
-            driver.Close();
-
-            #endregion
-
+            //Console.Write("Automated Test is started");
         }
+
+        [SetUp]
+        public void Initialize()
+        {
+            //Go to the url
+            driver.Navigate().GoToUrl("https://staging.evermosa2z.com/login");
+        }
+
+        [Test] //Attribute NUnit untuk bikin test
+        public void Login()
+        {
+            IWebElement element = driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div[2]/form/label[1]/span[2]/input"));
+            element.SendKeys("6289888111325");
+
+            element = driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div[2]/form/label[2]/span[2]/input"));
+            element.SendKeys("evm123!!");
+
+            element = driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div[3]/button"));
+            element.Click();
+
+            Thread.Sleep(5000);
+
+            //element = driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div[1]/div[1]"));
+            //element.Displayed();
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+        }
+
+        //private void readRow(object sender, EventArgs e)
+        //{
+        //    string test;
+
+        //    HSSFWorkbook hssfwb;
+        //    using (FileStream file = new FileStream(@"D:\Work\Automated Test\Selenium\npoi-test.xls", FileMode.Open, FileAccess.Read))
+        //    {
+        //        hssfwb = new HSSFWorkbook(file);
+        //    }
+
+        //    ISheet sheet = hssfwb.GetSheet("Sheet1");
+        //    for (int row = 0; row <= sheet.LastRowNum; row++)
+        //    {
+        //        if (sheet.GetRow(row) != null) //null is when the row only contains empty cells 
+        //        {
+        //            test = string.Format("Row {0} = {1}", row, sheet.GetRow(row).GetCell(0).StringCellValue);
+        //        }
+        //    }
+        //}
 
     }
 
